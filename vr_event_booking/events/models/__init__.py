6f5a4489ea6aa44083857ac174cdb2476272cdad
terafora <charlie.stone649@gmail.com>
2024-11-17 10:40:08 +0000

    Create initial views

---
+++ vr_event_booking/events/models/__init__.py
@@ -0,0 +1,3 @@
+from .event import Event
+from .ticket_type import TicketType
+from .booking import Booking
--- vr_event_booking/events/views.py
+++ vr_event_booking/events/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Event, TicketType
+from .serializers import EventSerializer, TicketTypeSerializer
 
-# Create your views here.
+class EventListAPI(APIView):
+    def get(self, request):
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+class TicketTypeAPI(APIView):
+    def get(self, request, event_id):
+        ticket_types = TicketType.objects.filter(event_id=event_id)
+        serializer = TicketTypeSerializer(ticket_types, many=True)
+        return Response(serializer.data)
